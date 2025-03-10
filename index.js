486cb8a1c2f9c9d1b4c83e4466f544c6a4d4516b
Arnold Mosha <arnoldmosha60@gmail.com>
2025-03-10 19:47:30 +0300

    final commit

--- index.js
+++ index.js
@@ -23,6 +23,51 @@ const resolvers = {
         review(_, args) {
             return _db.reviews.find((review) => review.id === args.id)
         }
+    },
+    Game: {
+        reviews(parent) {
+            return _db.reviews.filter((r) => r.game_id === parent.id)
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return _db.reviews.filter((a) => a.author_id === parent.id)
+        }
+    },
+    Review: {
+        author(parent) {
+            return _db.authors.find((a) => a.id === parent.author_id)
+        },
+        game(parent) {
+            return _db.games.find((g) => g.id === parent.game_id)
+        }
+    },
+    Mutation: {
+        deleteGame(_, args) {
+            _db.games = _db.games.filter((game) => game.id !== args.id)
+            return _db.games
+        },
+        addGame(_, args) {
+            const newGame = {
+                ...args.game,
+                id: Math.floor(Math.random() * 1000).toString()
+            }
+            _db.games.push(newGame)
+
+            return newGame
+        },
+        updateGames(_, args) {
+            _db.games = _db.games.map((game) => {
+                if (game.id === args.id) {
+                    return {
+                        ...game,
+                        ...args.edits
+                    }
+                }
+                return game
+            })
+            return _db.games.find((game) => game.id === args.id)
+        }
     }
 }
 
